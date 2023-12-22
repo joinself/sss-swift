caaa134888894206672b16f509a6f83c44fe507f
Long Pham <philong.bmt@gmail.com>
2023-12-22 20:08:08 +0700

    rename library name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ShamirSecretSharing",
+    name: "SSSSwift",
     products: [
         .library(name: "SSSSwift", targets: ["libsss"]),
     ],
