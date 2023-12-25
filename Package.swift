ce30398fce593d8c0bee0796a157e8584a7ed34d
Long Pham <philong.bmt@gmail.com>
2023-12-25 17:56:19 +0700

    add dependency to lib `sssswift`

--- Package.swift
+++ Package.swift
@@ -6,16 +6,16 @@ import PackageDescription
 let package = Package(
     name: "SSSSwift",
     products: [
-        .library(name: "SSSSwift", targets: ["libsss"]),
+        .library(name: "SSSSwift", targets: ["libsss", "SSSSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-    .target(name: "libsss"),
-    .target(name: "ShamirSecretSharing", dependencies: ["libsss"]),
-    .target(name: "SSSSwift", dependencies: ["libsss"]),
-    .testTarget(name: "ShamirSecretSharingTests", dependencies: ["SSSSwift"])
+        .target(name: "libsss"),
+        .target(name: "ShamirSecretSharing", dependencies: ["libsss"]),
+        .target(name: "SSSSwift", dependencies: ["libsss"]),
+        .testTarget(name: "ShamirSecretSharingTests", dependencies: ["SSSSwift"])
     ]
 )
