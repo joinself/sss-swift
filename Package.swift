be93e0816845f7e126a44f007c4be254d945d33a
Long Pham <philong.bmt@gmail.com>
2025-06-30 18:33:45 +0700

    set swift tools to `5.4.0`

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:6.1.0
+// swift-tools-version:5.4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SSSSwift",
+    name: "S3Swift",
     products: [
         .library(name: "SSSSwift", targets: ["libsss", "SSSSwift"]),
     ],
